6474aa4f477be10a5baf11e7d72592d203f8bc12
CabraKill <raphaeldesouza@outlook.com>
2019-10-01 10:18:36 -0300

    morning update 01-10-2019

--- postITDesktop/lib/pages/WallScreen.dart
+++ postITDesktop/lib/pages/WallScreen.dart
@@ -4,22 +4,39 @@ import 'package:example_flutter/widgets/PostIt.dart';
 import 'package:flutter/material.dart';
 
 class WallScreen extends StatefulWidget {
-
   @override
   _WallScreenState createState() => _WallScreenState();
 }
 
 class _WallScreenState extends State<WallScreen> {
-  NotesArea notesAero = NotesArea([Notes("teste de estático",[10,08,19],[15,08,19],0)], [Notes("teste dinâmico",[10,08,19],[15,08,19],1)], [Notes("teste dinâmico",[10,08,19],[15,08,19],1)]);
-  List<Notes> notesCargas = [Notes("fazer asaaaa",[1,08,19],[11,08,19],1)];
-  List<Notes> notesControle = [Notes("testar fuselagem",[9,08,19],[22,08,19],2)];
+  NotesArea notesAero = NotesArea([
+    Notes(
+        "teste de estático", DateTime(2019, 08, 10), DateTime(2019, 08, 15), 0)
+  ], [
+    Notes("teste dinâmico", DateTime(2019, 08, 10), DateTime(2019, 08, 15), 1)
+  ], [
+    Notes("teste dinâmico", DateTime(2019, 08, 10), DateTime(2019, 08, 15), 1)
+  ]);
+  List<Notes> notesCargas = [
+    Notes("fazer asaaaa", DateTime(2019, 08, 1), DateTime(2019, 08, 11), 1),
+    Notes("fazer cronograma", DateTime(2019, 08, 1), DateTime(2019, 08, 11), 0),
+    Notes("fazer cronograma", DateTime(2019, 08, 1), DateTime(2019, 08, 11), 0),
+    Notes("fazer cronograma", DateTime(2019, 08, 1), DateTime(2019, 08, 11), 0)
+  ];
+  List<Notes> notesControle = [
+    Notes("testar fuselagem", DateTime(2019, 08, 9), DateTime(2019, 10, 22), 2)
+  ];
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: new AppBar(
         title: new Text("Wall"),
       ),
-      body: Wall([notesCargas,notesCargas,notesControle]),//Center(child: MyGrid(context)),
+      body: Wall([
+        notesCargas,
+        notesControle,
+        notesControle
+      ]), //Center(child: MyGrid(context)),
       backgroundColor: Colors.grey[700],
       //bottomNavigationBar: AddBottomBar(),
     );
